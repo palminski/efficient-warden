8f13f2b6ea3358f90d8cbac65678545d390c06c8
palminski <williambolls98@gmail.com>
2023-06-09 14:28:17 -0700

    set up with Tauri instead of electron

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+import { useState } from 'react';
+
+import { Referrals, Settings } from './pages/index'
+import { Nav } from './components';
+
+
+function App() {
+  //States
+  const [pageSelected, setPageSelected] = useState('referrals');
+
+  //return JSX
+  return (
+    <div className="App">
+
+      <Nav pageSelected={pageSelected} setPageSelected={setPageSelected} />
+
+      {pageSelected === 'referrals' && <Referrals />}
+      {pageSelected === 'settings' && <Settings />}
+
+    </div>
+  );
+}
+
+export default App;
